9aaaf9d44a7202e11f3e59526f08ed6c0200ef96
JeyaVishnu21 <vishnu21072007_bcs28@mepcoeng.ac.in>
2026-07-24 10:38:45 +0530

    Update 3513. Number of unique XOR triplets.cpp

--- 3513. Number of unique XOR triplets.cpp
+++ 3513. Number of unique XOR triplets.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        const unsigned n=nums.size();
-        return (n<3)?n:1<<(32-countl_zero(n));
+        int n = nums.size();
+        if(n<=2) return n;
+        int ans = 1;
+        while (ans <= n)
+            ans <<= 1;
+
+        return ans;
     }
 };
